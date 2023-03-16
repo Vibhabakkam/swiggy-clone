6966fb1547ce3037135f3f30bce6d54bf6422fd9
Vibhabakkam <vibhabakkam22@gmail.com>
2023-03-16 15:25:38 +0530

    Create App.jsx

---
+++ swiggy/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Homepage from './Component/Homepage';
+import SignUp from './Component/SignUp';
+
+
+function App() {
+  return (
+    <div>
+      <Homepage/>
+    </div>
+  );
+}
+
+export default App;
