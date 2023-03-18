7bbc64f5b5e53eee731e1e43083fe1fd78ae1a44
Vibhabakkam <vibhabakkam22@gmail.com>
2023-03-18 19:31:41 +0530

    Create Home.jsx

---
+++ swiggy/src/Component/Home.jsx
@@ -0,0 +1,45 @@
+import "./Component.css";
+import { useState } from "react";
+
+function Home() {
+
+    return (
+        <>
+            <div id="screendiv">
+                <div id="snavdiv">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div id="sbodydiv">
+                    <div>
+                        <img src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_520,h_520/rng/md/carousel/production/pneknawbadtvceqzwiep" alt="" />
+                        <img src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_520,h_520/rng/md/carousel/production/zpkkdkmvlj5cuvqbc50t" alt="" />
+                        <img src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_520,h_520/rng/md/carousel/production/awurei8ypqkafoqay9ym" alt="" />
+                        <img src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_520,h_520/rng/md/carousel/production/ifi2lbzxeu1hvsqrsip3" alt="" />
+                    </div>
+                </div>
+
+
+
+            </div>
+
+
+
+        </>
+
+
+
+    )
+
+
+
+
+
+
+
+}
+export default Home;
