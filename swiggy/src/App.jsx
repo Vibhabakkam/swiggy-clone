975eb48f3d852e8732d7054052ea0e3fdeeae39e
Vibhabakkam <vibhabakkam22@gmail.com>
2023-03-18 19:31:49 +0530

    Update App.jsx

--- swiggy/src/App.jsx
+++ swiggy/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import Homepage from './Component/Homepage';
 import SignUp from './Component/SignUp';
+import Home from './Component/Home';
 
 
 function App() {
   return (
     <div>
-      <Homepage/>
+      <Home/>
     </div>
   );
 }
