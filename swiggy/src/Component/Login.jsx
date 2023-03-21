4e0499116d7ab0c83d143e72fedfcf08c0c6d02d
Vibhabakkam <vibhabakkam22@gmail.com>
2023-03-21 22:14:53 +0530

    Update Login.jsx

--- swiggy/src/Component/Login.jsx
+++ swiggy/src/Component/Login.jsx
@@ -1,35 +1,35 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-function Login() {
-    
+function Login(props) {
+
     const [userData, setUserData] = useState({ email: "", password: "" });
     // console.log(userData, "userData check here");
     const router2 = useNavigate();
 
     function handleSubmit(e) {
         e.preventDefault();
-        var dataFromLs =JSON.parse(localStorage.getItem("userData"));
+        var dataFromLs = JSON.parse(localStorage.getItem("userData"));
 
-    var flag= false;
+        var flag = false;
 
-    for (var i=0 ; i< dataFromLs.length ; i++){
-        if(dataFromLs[i].email === userData.email  && dataFromLs[i].password === userData.password){
-            flag =true;
+        for (var i = 0; i < dataFromLs.length; i++) {
+            if (dataFromLs[i].email === userData.email && dataFromLs[i].password === userData.password) {
+                flag = true;
+            }
         }
-    }
 
-    if(flag){
-       
-        var user ={};
-        user["current-user-email"]= userData.email;
-        localStorage.setItem("currentUser",JSON.stringify(user));
-        router2('/Home');
-        alert("Login sucessful");
+        if (flag) {
 
-    }
-    else{
-        alert("Email or Password does not match");
-    }
+            var user = {};
+            user["current-user-email"] = userData.email;
+            localStorage.setItem("currentUser", JSON.stringify(user));
+            router2('/Home');
+            alert("Login sucessful");
+
+        }
+        else {
+            alert("Email or Password does not match");
+        }
     }
 
     function updatingData(e) {
@@ -42,17 +42,33 @@ function Login() {
     return (
 
         <>
-            <form onSubmit={(event) => handleSubmit(event)}>
-                <label>Email</label><br />
-                <input onChange={updatingData} name='email' value={userData.email} type="email" placeholder="Type your Email" /><br />
-                <label>Password</label><br />
-                <input onChange={updatingData} name='password' value={userData.password} type="password" placeholder="Type your Passwrd" /><br />
-                <button>Have a referral code?</button><br />
-                <input type="submit" value="Login" />
-                <div>By creating an account, I accept the Terms & Conditions & Privacy Policy</div>
+            <div id="login">
+                <div id="register">
+                    <div onClick={props.onClose}> X </div>
+                    <div id="top">
 
-            </form>
+                        <div>
+                            <div>Login</div>
+                            <div>
 
+                            </div>
+                        </div>
+                        <div>
+                            <img src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/Image-login_btpq7r" alt="img" />
+                        </div>
+                    </div>
+                    <form onSubmit={(event) => handleSubmit(event)}>
+                        <label>Email</label><br />
+                        <input onChange={updatingData} name='email' value={userData.email} type="email" placeholder="Type your Email" /><br />
+                        <label>Password</label><br />
+                        <input onChange={updatingData} name='password' value={userData.password} type="password" placeholder="Type your Passwrd" /><br />
+                        <button>Have a referral code?</button><br />
+                        <input id="Rsubmit" type="submit" value="Login" />
+                        <div>By creating an account, I accept the Terms & Conditions & Privacy Policy</div>
+
+                    </form>
+                </div>
+            </div>
         </>
     )
 
