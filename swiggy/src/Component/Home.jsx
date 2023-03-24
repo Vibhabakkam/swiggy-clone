47beb50351903631e0a3072aedda3b004da4a91b
Vibhabakkam <vibhabakkam22@gmail.com>
2023-03-24 20:37:16 +0530

    Update Home.jsx

--- swiggy/src/Component/Home.jsx
+++ swiggy/src/Component/Home.jsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Home.css";
-import Navbar from "./Navbar";
-
+import Navbar from "./Navbar"
 
 function Home() {
     const[data, setData] = useState();
