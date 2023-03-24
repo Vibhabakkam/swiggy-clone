3e670ee90ad5b89147b7ae42882468a4109e96c5
Vibhabakkam <vibhabakkam22@gmail.com>
2023-03-24 20:37:12 +0530

    Update index.js

--- swiggy/src/index.js
+++ swiggy/src/index.js
@@ -4,7 +4,31 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import  {Toaster } from 'react-hot-toast';
+<Toaster
+  position="top-center"
+  reverseOrder={false}
+  gutter={8}
+  containerClassName=""
+  containerStyle={{}}
+  toastOptions={{
+    // Define default options
+    className: '',
+    duration: 5000,
+    style: {
+      background: '#363636',
+      color: '#fff',
+    },
 
+    // Default options for specific types
+    success: {
+      duration: 3000,
+      theme: {
+        primary: 'green',
+        secondary: 'black',
+      },
+    },
+  }} ></Toaster>
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
